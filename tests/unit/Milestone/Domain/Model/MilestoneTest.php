9c7827e6a5ae11946db4c5ef65f0e47db8c44238
Boris Grishenko <arczinosek@gmail.com>
2025-06-29 22:02:21 +0200

    Update Milestone entity tests

--- tests/unit/Milestone/Domain/Model/MilestoneTest.php
+++ tests/unit/Milestone/Domain/Model/MilestoneTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\unit\Milestone\Domain\Model;
 
+use App\Milestone\Domain\Exception\TooManyAttachmentsException;
 use App\Milestone\Domain\Model\Milestone;
 use DateTimeImmutable;
 use PHPUnit\Framework\Attributes\Test;
@@ -11,6 +12,8 @@ use PHPUnit\Framework\TestCase;
 
 final class MilestoneTest extends TestCase
 {
+    private const ATTACHMENTS_LIMIT = 20;
+
     #[Test]
     public function setTitleShouldThrowExceptionWhenTitleIsTooShort(): void
     {
@@ -30,6 +33,15 @@ final class MilestoneTest extends TestCase
     }
 
     #[Test]
+    public function setTitleHappyPath(): void
+    {
+        $milestone = new Milestone();
+        $milestone->setTitle('some title');
+
+        $this->assertNotNull($milestone->getUpdatedAt());
+    }
+
+    #[Test]
     public function setPeriodShouldThrowExceptionWhenFinishDateIsBeforeStartDate(): void
     {
         $milestone = new Milestone();
@@ -40,5 +52,86 @@ final class MilestoneTest extends TestCase
             new DateTimeImmutable('2025-06-16'),
             new DateTimeImmutable('2025-06-10')
         );
+    }
+
+    #[Test]
+    public function setPeriodHappyPath(): void
+    {
+        $milestone = new Milestone();
+        $startDate = new DateTimeImmutable('2025-06-29 21:39:00');
+        $finishDate = new DateTimeImmutable('2025-06-29 21:40:03');
+
+        $milestone->setPeriod($startDate, $finishDate);
+
+        $this->assertSame($startDate, $milestone->getStartDate());
+        $this->assertSame($finishDate, $milestone->getFinishDate());
+        $this->assertNotNull($milestone->getUpdatedAt());
+    }
+
+    #[Test]
+    public function setDescriptionHappyPath(): void
+    {
+        $milestone = new Milestone();
+        $description = 'some description';
+
+        $milestone->setDescription($description);
+
+        $this->assertSame($description, $milestone->getDescription());
+        $this->assertNotNull($milestone->getUpdatedAt());
+    }
+
+    #[Test]
+    public function addAttachmentHappyPath(): void
+    {
+        $milestone = new Milestone();
+
+        $attachment = $milestone->addAttachment(
+            '/storage/milestone/uploaded_file.jpg',
+            'image/jpeg',
+            666,
+            'awesome_summer.jpg',
+            'awesome summer \'25'
+        );
+
+        $this->assertNotNull($milestone->getUpdatedAt());
+        $this->assertSame($milestone, $attachment->getMilestone());
+        $this->assertSame('/storage/milestone/uploaded_file.jpg', $attachment->getFilePath());
+        $this->assertSame('image/jpeg', $attachment->getFileMimeType());
+        $this->assertSame(666, $attachment->getFileSizeBytes());
+        $this->assertSame('awesome_summer.jpg', $attachment->getOriginalFileName());
+        $this->assertSame('awesome summer \'25', $attachment->getDescription());
+    }
+
+    #[Test]
+    public function addAttachmentShouldThrowExceptionWhenLimitOfAttachmentsAchieved(): void
+    {
+        $expectedException = TooManyAttachmentsException::create(
+            '/storage/milestone/uploaded_file_z.jpg',
+            self::ATTACHMENTS_LIMIT
+        );
+        $milestone = new Milestone();
+
+        for ($i = 0; $i < self::ATTACHMENTS_LIMIT; $i++) {
+            $milestone->addAttachment(
+                '/storage/milestone/uploaded_file.jpg',
+                'image/jpeg',
+                ($i + 1) * 2,
+                'awesome_summer.jpg',
+            );
+        }
+
+        $lastUpdatedAt = $milestone->getUpdatedAt();
+
+        $this->expectExceptionObject($expectedException);
+
+        $milestone->addAttachment(
+            '/storage/milestone/video.wav',
+            'image/wav',
+            987,
+            'awesome_summer.wav',
+        );
+
+        $this->assertCount(self::ATTACHMENTS_LIMIT, $milestone->getAttachments());
+        $this->assertSame($lastUpdatedAt, $milestone->getUpdatedAt());
     }
 }
