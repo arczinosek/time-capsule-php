562219eaed51b9255e5606a7aa26635fef3267c6
Boris Grishenko <arczinosek@gmail.com>
2025-07-11 20:30:43 +0200

    Add data from database to wall

--- src/Milestone/Presentation/Http/Controller/FrontController.php
+++ src/Milestone/Presentation/Http/Controller/FrontController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Milestone\Presentation\Http\Controller;
 
+use App\Milestone\Application\Handler\ListMilestonesHandler;
+use App\Milestone\Application\Query\ListMilestonesQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,6 +13,11 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/', name: 'app_')]
 class FrontController extends AbstractController
 {
+    public function __construct(
+        private readonly ListMilestonesHandler $listMilestonesHandler,
+    ) {
+    }
+
     #[Route('/', name: 'home', methods: ['GET'])]
     public function home(): Response
     {
@@ -20,6 +27,12 @@ class FrontController extends AbstractController
     #[Route('/wall', name: 'wall', methods: ['GET'])]
     public function milestones(): Response
     {
-        return $this->render('wall/index.html.twig');
+        $milestones = $this->listMilestonesHandler->handle(
+            new ListMilestonesQuery()
+        );
+
+        return $this->render('wall/index.html.twig', [
+            'milestones' => $milestones,
+        ]);
     }
 }
